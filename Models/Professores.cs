5bed2e04b6ab7c232fddfa543e26f735a0e30b30
fernandoareias <nando.calheirosx@gmail.com>
2021-08-11 17:33:07 -0300

    Private props e cria relacionamento com curso

--- Models/Professores.cs
+++ Models/Professores.cs
@@ -1,5 +1,4 @@
-
-
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,11 +10,11 @@ namespace CursoIdiomasAPI.Models
     {
 
         [Key]
-        public int Id { get; set; }
+        public int Id { get; private set; }
 
         [Required(ErrorMessage = "Esse campo é obrigatório")]
         [MaxLength(60, ErrorMessage = "Esse campo deve conter até 60 caracteres")]
-        public string Name { get; set; }
+        public string Nome { get; set; }
 
         [Required(ErrorMessage = "Esse campo é obrigatório")]
         [MaxLength(120, ErrorMessage = "Esse campo deve conter até 120 caracteres")]
@@ -23,10 +22,10 @@ namespace CursoIdiomasAPI.Models
 
         [Required(ErrorMessage = "Esse campo é obrigatório")]
         [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
-        public decimal ValorHora { get; set; }
+        public decimal SalarioMes { get; set; }
 
-        //   [Required(ErrorMessage = "Esse campo é obrigatório")]
-        // public List<Turma> Turmas { get; set; }
-
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        public int CursoId { get; set; }
+        public Curso Cursos { get; private set; }
     }
 }
