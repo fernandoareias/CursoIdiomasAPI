3f95a39eb94db19e443015b296dff48392ce2662
fernandoareias <nando.calheirosx@gmail.com>
2021-08-14 13:52:09 -0300

    Modelo Mensalidade

--- Models/Mensalidade.cs
+++ Models/Mensalidade.cs
@@ -11,16 +11,18 @@ namespace CursoIdiomasAPI.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; private set; }
-        public string URL { get; private set; }
 
         [Required(ErrorMessage = "Esse campo é obrigatório")]
-        public Guid MatriculaId { get; set; }
-        public Matricula Matricula { get; private set; }
+        public Guid MatriculaId { get; private set; }
+
         [Required(ErrorMessage = "Esse campo é obrigatório")]
         public string Vencimento { get; set; }
         [Required(ErrorMessage = "Esse campo é obrigatório")]
         [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Valor { get; set; }
+
+        public void SetId(Guid id) => Id = id;
+        public void SetMatriculaId(Guid id) => MatriculaId = id;
     }
 }
