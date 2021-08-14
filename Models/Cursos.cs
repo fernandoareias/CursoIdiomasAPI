2ebc21c9dfb77ff96ac2bb22378488dc46c12eb7
fernandoareias <nando.calheirosx@gmail.com>
2021-08-14 13:51:05 -0300

    Removido var URL

--- Models/Cursos.cs
+++ Models/Cursos.cs
@@ -7,15 +7,10 @@ namespace CursoIdiomasAPI.Models
     [Table("Cursos")]
     public class Curso
     {
-        public Curso()
-        {
-            Id = Guid.NewGuid();
-            URL = Id.ToString("N");
-        }
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; private set; }
-        public string URL { get; private set; }
 
         [Required(ErrorMessage = "Esse campo é obrigatório")]
         [MaxLength(80, ErrorMessage = "Esse campo deve conter até 120 caracteres")]
@@ -24,6 +19,7 @@ namespace CursoIdiomasAPI.Models
         [Required(ErrorMessage = "Esse campo é obrigatório")]
         [Range(1, int.MaxValue, ErrorMessage = "A carga horária deve ser maior que zero")]
         public int CargaHoraria { get; set; }
+        public void SetId(Guid id) => Id = id;
 
     }
 }
