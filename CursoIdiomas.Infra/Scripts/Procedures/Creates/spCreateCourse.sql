53bad7fd4263cdf3d2f6f852ca42355399672a13
fernandoareias <nando.calheirosx@gmail.com>
2021-09-03 19:29:28 -0300

    Add news atributes

--- CursoIdiomas.Infra/Scripts/Procedures/Creates/spCreateCourse.sql
+++ CursoIdiomas.Infra/Scripts/Procedures/Creates/spCreateCourse.sql
@@ -2,16 +2,19 @@
 CREATE PROCEDURE spCreateCourse
     @Id UNIQUEIDENTIFIER,
     @Name VARCHAR(80),
+    @Level TINYINT,
     @Workload INT
 AS
 INSERT INTO [Courses]
     (
     [Id],
     [Name],
+    [Level],
     [Workload]
     )
 VALUES(
         @Id,
         @Name,
+        @Level,
         @Workload
     )
