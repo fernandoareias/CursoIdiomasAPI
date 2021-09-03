56991f0c7669679e0419b59eef2897a04413cc98
fernandoareias <nando.calheirosx@gmail.com>
2021-09-03 19:31:34 -0300

    Base Tests

---
+++ CursoIdiomas.Tests/Entities/Course.cs
@@ -0,0 +1,16 @@
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CursoIdiomas.Tests
+{
+    [TestClass]
+    public class CourseTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ CursoIdiomas.Tests/Entities/TeacherTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CursoIdiomas.Tests
+{
+    [TestClass]
+    public class TeacherTests
+    {
+        // Deve retornar se professor for valido
+        [TestMethod]
+        public void ShouldReturnIfTeacherIsValid()
+        {
+            Assert.Fail();
+        }
+
+        // Deve retornar se professor for invalido
+        [TestMethod]
+        public void ShouldReturnIfTeacherIsInvalid()
+        {
+            Assert.Fail();
+        }
+    }
+}
