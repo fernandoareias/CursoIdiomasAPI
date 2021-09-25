28a63252b3de0744c598e9319cdd03ccaeca7aa0
fernandoareias <nando.calheirosx@gmail.com>
2021-09-25 16:49:15 -0300

    CRUD Cursos

--- 0 - API/Controllers/CursosControllers.cs
+++ 0 - API/Controllers/CursosControllers.cs
@@ -1,4 +1,5 @@
-﻿using CursoIdiomas.Application.Cursos.Interfaces;
+﻿using CursoIdiomas.Application.Cursos.DTO;
+using CursoIdiomas.Application.Cursos.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -32,5 +33,63 @@ namespace CursoIdiomas.API.Controllers
 
             return Ok(result);
         }
+
+
+        [HttpGet]
+        [Route("cursos/{idCurso}")]
+        public async Task<ActionResult> GetById(Guid idCurso) {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _cursoAppService.ObterCurso(idCurso);
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("cursos")]
+        public async Task<ActionResult> Post([FromBody]CursoDTO model) {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _cursoAppService.RegistrarCurso(model);
+
+            if(result == null) 
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+
+        [HttpPut]
+        [Route("cursos/{idCurso}")]
+        public async Task<ActionResult> Put(Guid idCurso, [FromBody] CursoDTO model) {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _cursoAppService.AtualizarCurso(idCurso, model);
+
+            if (result == null)
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("curso/{idCurso}")]
+        public async Task<ActionResult> Delete(Guid idCurso) {
+
+            var result = await _cursoAppService.Remover(idCurso);
+            if (result == null)
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+
+
     }
 }
