695ec99bae30c90e1d9acb696ae35ce102dab07a
fernandoareias <nando.calheirosx@gmail.com>
2021-09-25 13:27:30 -0300

    Controller Cursos GetAll

---
+++ 0 - API/Controllers/CursosControllers.cs
@@ -0,0 +1,36 @@
+﻿using CursoIdiomas.Application.Cursos.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoIdiomas.API.Controllers
+{
+    [Route("v1/api/")]
+    [ApiController]
+    public class CursosControllers : ControllerBase
+    {
+        private readonly ICursoAppServices _cursoAppService;
+
+        public CursosControllers(ICursoAppServices cursoAppService)
+        {
+            _cursoAppService = cursoAppService;
+        }
+
+
+        [HttpGet]
+        [Route("cursos")]
+        public async Task<ActionResult> GetAll()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _cursoAppService.GetAll();
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
