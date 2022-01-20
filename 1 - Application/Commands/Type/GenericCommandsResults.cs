c14f6d1a662c88baea60782f16b8058d667dfffb
Fernando <nando.calheirosx@gmail.com>
2022-01-20 18:24:20 -0300

    Update arch

--- 1 - Application/Commands/Type/GenericCommandsResults.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CursoIdiomas.API.Controllers {
-    public class GenericCommandsResults<T> {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public T Data { get; set; }
-    }
-}
